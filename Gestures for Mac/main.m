801eece96b194afd8c15fd73d7841e413ababbb9
mz2 <matias.piipari@gmail.com>
2014-01-03 14:02:40 +0700

    OSX targets going again after renaming & recreating target.

---
+++ Gestures for Mac/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Gestures for Mac
+//
+//  Created by Matias Piipari on 03/01/2014.
+//  Copyright (c) 2014 de.ur. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
